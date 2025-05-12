13e0213ddb53bc5217e487f62072a362cdabbd45
Peter Kirschner <peter@klib.io>
2025-05-12 19:19:56 +0200

    adding version output

--- src/main/java/io/klib/mvn/deploy/java/App.java
+++ src/main/java/io/klib/mvn/deploy/java/App.java
@@ -1,7 +1,11 @@
 package io.klib.mvn.deploy.java;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * Hello world!
@@ -12,5 +16,39 @@ public class App {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
 		String timestamp = LocalDateTime.now().format(formatter);
 		System.out.println("Hello Maven Central deployment " + timestamp);
+		System.out.println("Implementation-Version: " + readImplementationVersion());
+
+	}
+
+	private static String readImplementationVersion() {
+		String rv = "This class is not inside a JAR file.";
+		try {
+			// Get the location of the current class file (JAR file containing this class)
+			String classPath = App.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+			// If the class is inside a JAR, the location will have a ".jar" extension
+			if (classPath.endsWith(".jar")) {
+				// Open the JAR file
+				try (JarFile jarFile = new JarFile(classPath)) {
+					// Get the manifest file from the JAR
+					Manifest manifest = jarFile.getManifest();
+
+					// Get the main attributes from the manifest
+					Attributes mainAttributes = manifest.getMainAttributes();
+
+					// Read the Implementation-Version
+					String version = mainAttributes.getValue("Implementation-Version");
+
+					if (version != null) {
+						rv = "Implementation-Version: " + version;
+					} else {
+						rv = "Implementation-Version not found in the manifest.";
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return rv;
 	}
 }
